b18bf914626b445bbb81cbd2fc93fb1183f6b88a
njugunanduati@gmail.com <njugunanduati@gmail.com>
2018-03-26 22:05:53 +0300

    added the reset password functionality and improved the ui

--- config.py
+++ config.py
@@ -4,3 +4,15 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'just trust me'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:jamonjuguna@localhost/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['njugunanduati@gmail.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    POSTS_PER_PAGE = 25
